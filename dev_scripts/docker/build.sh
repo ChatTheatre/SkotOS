49057709662a00a7bf24d84df6a2268c21225638
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-10-29 16:22:43 +0000

    Docker now pulls from Docker Hub and opens a new log window.

--- dev_scripts/docker/build.sh
+++ dev_scripts/docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache -t skotos .
+docker build --no-cache -t noahgibbs/skotos .
--- dev_scripts/start_server.sh
+++ dev_scripts/start_server.sh
@@ -7,13 +7,16 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 cd ..
 
+DOCKER_TAG="noahgibbs/skotos:earliest"
+
 if (docker ps -a | grep sk)
 then
     # Restart the stopped container, if it's stopped
     docker start sk || echo "Maybe the Docker container is already running?"
 else
     # Create a new container
-    docker run -p 10800:10800 -p 10900:10900 -p 10802:10802 -p 10080:10080 -p 10443:10443 --mount type=bind,src="$(pwd)/log",target="/var/log" --detach --name sk skotos
+    docker pull noahgibbs/skotos
+    docker run -p 10800:10800 -p 10900:10900 -p 10802:10802 -p 10080:10080 -p 10443:10443 -p 10098:10098 -p 10099:10099 --mount type=bind,src="$(pwd)/log",target="/var/log" --detach --name sk noahgibbs/skotos
 fi
 docker ps -a | grep sk && echo "You are already running a Docker container! Run stop_server.sh to remove it."
 
@@ -22,7 +25,7 @@ docker ps -a | grep sk && echo "You are already running a Docker container! Run 
 #    https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/osx/osx.plugin.zsh
 
 # Open iTerm/terminal window showing DGD process log
-open -a $TERM_PROGRAM dev_scripts/show_dgd_logs.sh
+open -a Terminal -n dev_scripts/show_dgd_logs.sh
 
 # Wait until SkotOS is booted and responsive
 while true
@@ -30,15 +33,12 @@ do
     sleep 5
     echo "Checking if SkotOS is responsive yet. The first run can take five to ten minutes until everything is working..."
     curl -L http://localhost:10080/SAM/Prop/Theatre:Web:Theatre/Index > /tmp/test_skotos.html || echo "(curl failed)"
-    grep "Skotos" /tmp/test_skotos.html && break
+    # Note: can sometimes get a "theatre not found" transient error early in bootup
+    grep "Log in to Skotos" /tmp/test_skotos.html && break
 done
 
 cat dev_scripts/post_install_instructions.txt
 
 open -a "Google Chrome" "http://localhost:10080/SAM/Prop/Theatre:Web:Theatre/Index"
+#open -a Terminal -n "telnet localhost 10098"
 # TODO: open iTerm/terminal window to telnet port
-
-
-##################
-# TODO: DockerHub image: https://docs.docker.com/get-started/part3/
-# REF: https://docs.docker.com/storage/bind-mounts/
