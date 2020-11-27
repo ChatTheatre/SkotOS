34d43cf8cbb35700c7268924c5a87f16c86439b6
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-11-27 13:58:29 +0000

    DGD and thin-auth are running. DGD is serving files. But some of the DNS and NGinX configuration is clearly still wrong.

--- dev_scripts/stackscript/linode_stackscript.sh
+++ dev_scripts/stackscript/linode_stackscript.sh
@@ -11,29 +11,38 @@
 #
 # <UDF name="hostname" label="Short Hostname" example="Example: my-awesome-game"/>
 # HOSTNAME=
-# <UDF name="fqdn" label="Fully Qualified Hostname" example="Example: my-awesome-game.my-domain.com"/>
-# FQDN=
+# <UDF name="fqdn_client" label="Fully Qualified Client Hostname" example="Example: my-awesome-game.my-domain.com"/>
+# FQDN_CLIENT=
+# <UDF name="fqdn_login" label="Fully Qualified Thin-Auth Hostname" example="Example: my-awesome-game.my-domain.com"/>
+# FQDN_LOGIN=
 # <UDF name="userpassword" label="Deployment User Password" example="Password for the host deployment account." />
 # USERPASSWORD=
+# <UDF name="email" label="Support and PayPal Email" default="" example="Email for game support and for PayPal payments, if configured" optional="false" />
+# EMAIL=
 # <UDF name="ssh_key" label="SSH Key" default="" example="SSH Key for automated logins to host's deployment account." optional="true" />
 # SSH_KEY=
-# <UDF name="skotosgiturl" label="Skotos Git URL" default="https://github.com/ChatTheatre/SkotOS" example="SkotOS Git URL to clone for your game." optional="false" />
+# <UDF name="skotos_git_url" label="Skotos Git URL" default="https://github.com/ChatTheatre/SkotOS" example="SkotOS Git URL to clone for your game." optional="false" />
 # SKOTOS_GIT_URL=
-# <UDF name="dgdgiturl" label="Skotos DGD URL" default="https://github.com/ChatTheatre/dgd" example="SkotOS DGD URL to clone for your game." optional="false" />
+# <UDF name="dgd_git_url" label="DGD Git URL" default="https://github.com/ChatTheatre/dgd" example="DGD Git URL to clone for your game." optional="false" />
 # DGD_GIT_URL=
-
+# <UDF name="thinauth_git_url" label="Thin-Auth Git URL" default="https://github.com/ChatTheatre/thin-auth" example="Thin-Auth Git URL to clone for your game." optional="false" />
+# THINAUTH_GIT_URL=
 
 
 # Some differences from full real SkotOS setup:
 #
 # 1. No IP address whitelisting for SSH
-# 2. No DNS setup with multiple names - one IP address, one domain name, one host
+# 2. Only 2 DNS names, client and login (thin-auth)
 # 3. No hosting multiple games per host
+
+# Some URLs you can check when testing:
+#
+# * http://$FQDN_CLIENT/gables/gables.htm
 
 set -e  # Fail on error
 set -x
 
-# Force check for root, if you are not logged in as root then the script will not execute
+# Force check for root. If you are not logged in as root then the script will not execute
 if ! [ "$(id -u)" = 0 ]
 then
   echo "$0 - You need to be logged in as root!"
@@ -45,6 +54,16 @@ cd /
 # Output stdout and stderr to ~root files
 exec > >(tee -a /root/standup.log) 2> >(tee -a /root/standup.log /root/standup.err >&2)
 
+echo "Hostname: $HOSTNAME"
+echo "FQDN client: $FQDN_CLIENT"
+echo "FQDN login: $FQDN_LOGIN"
+echo "Support and PayPal email: $EMAIL"
+echo "USERPASSWORD/dbpassword: (not shown)"
+echo "SSH_KEY: (not shown)"
+echo "SkotOS Git URL: $SKOTOS_GIT_URL"
+echo "DGD Git URL: $DGD_GIT_URL"
+echo "Thin-Auth Git URL: $THINAUTH_GIT_URL"
+
 ####
 # 1. Update Hostname
 ####
@@ -55,13 +74,14 @@ echo $HOSTNAME > /etc/hostname
 # Set the variable $IPADDR to the IP address the new Linode receives.
 IPADDR=`hostname -I | awk '{print $1}'`
 
-echo "$0 - Set hostname as $FQDN ($IPADDR)"
-echo "$0 - TODO: Put $FQDN with IP $IPADDR in your main DNS file."
+echo "$0 - Set hostname as $FQDN_CLIENT ($IPADDR)"
+echo "$0 - TODO: Put $FQDN_CLIENT with IP $IPADDR in your main DNS file."
+echo "$0 - TODO: Put $FQDN_LOGIN with IP $IPADDR in your main DNS file."
 
 # Add localhost aliases
 
 echo "127.0.0.1    localhost" > /etc/hosts
-echo "127.0.1.1 $FQDN $HOSTNAME" >> /etc/hosts
+echo "127.0.1.1 $FQDN_CLIENT $FQDN_LOGIN $HOSTNAME" >> /etc/hosts
 
 echo "$0 - Set localhost"
 
@@ -69,7 +89,7 @@ echo "$0 - Set localhost"
 # 2. Bring Debian Up To Date
 ####
 
-echo "$0 - Starting Debian updates; this will take a while!"
+echo "$0 - Starting Debian updates; this may take a while!"
 
 # Make sure all packages are up-to-date
 apt-get update -y
@@ -83,9 +103,14 @@ apt-get -y install unattended-upgrades
 echo "$0 - Updated Debian Packages"
 
 # get uncomplicated firewall and deny all incoming connections except SSH
-#apt-get install ufw -y
-#ufw allow ssh
-#ufw enable
+apt-get install ufw -y
+ufw default allow outgoing
+ufw default deny incoming
+ufw allow ssh
+ufw allow 10000:10802/tcp  # for now, allow DGD incoming ports and tunnel ports
+ufw allow 80/tcp
+ufw allow 81/tcp
+ufw enable
 
 ####
 # 3. Set Up Initial User
@@ -118,7 +143,7 @@ curl -sL https://deb.nodesource.com/setup_9.x | bash -
 apt install nodejs npm -y
 
 # Thin-auth requirements
-#apt-get install mariadb-server libapache2-mod-php php php-mysql -y
+apt-get install mariadb-server php-fpm php php-mysql -y
 
 ####
 # Set up Directories, Groups and Ownership
@@ -141,21 +166,20 @@ then
     git pull
     popd
 else
-    #git clone ${SKOTOS_GIT_URL} /var/skotos
-    git clone https://github.com/ChatTheatre/SkotOS /var/skotos
+    git clone ${SKOTOS_GIT_URL} /var/skotos
+    #git clone https://github.com/ChatTheatre/SkotOS /var/skotos
     chgrp -R skotos /var/skotos
     chmod -R g+w /var/skotos
 fi
 
-# A nod to local development on the Linode
 if [ -d "/var/dgd" ]
 then
     pushd /var/dgd
     git pull
     popd
 else
-    #git clone ${DGD_GIT_URL} /var/dgd
-    git clone https://github.com/ChatTheatre/dgd /var/dgd
+    git clone ${DGD_GIT_URL} /var/dgd
+    #git clone https://github.com/ChatTheatre/dgd /var/dgd
 fi
 
 pushd /var/dgd/src
@@ -166,9 +190,15 @@ cat >>~skotos/crontab.txt <<EndOfMessage
 @reboot /var/skotos/dev_scripts/stackscript/start_dgd_server.sh &
 EndOfMessage
 
+/var/skotos/dev_scripts/stackscript/start_dgd_server.sh &
+
 ####
 # Set up NGinX
 ####
+
+# Fix a bad default in php-fpm
+sed 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' </etc/php/7.3/fpm/php.ini >/etc/php/7.3/fpm/php-fixed.ini
+mv /etc/php/7.3/fpm/php-fixed.ini /etc/php/7.3/fpm/php.ini
 
 cat >/etc/nginx/sites-available/skotos_game.conf <<EndOfMessage
 # skotos_game.conf
@@ -183,11 +213,37 @@ upstream gables {
 }
 
 server {
-    listen *:80 default;
+    listen *:80 default_server;
+
+    server_name $FQDN_CLIENT;
+    index index.php index.html index.htm ;
 
     root /var/www/html/client;
     location / {
         try_files \$uri \$uri/index.html \$uri.html =404;
+    }
+}
+
+server {
+    listen *:80 ;
+    listen *:81 default_server ;
+
+    server_name $FQDN_LOGIN ;
+
+    root /var/www/html/user ;
+    index index.php index.html index.htm ;
+
+    location / {
+        try_files \$uri \$uri/index.php \$uri/index.html \$uri.html =404 ;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf ;
+        fastcgi_pass unix:/run/php/php7.3-fpm.sock ;
+    }
+
+    location ~ /\.ht {
+        deny all;
     }
 }
 
@@ -208,7 +264,6 @@ rm -f /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/skotos_game.conf /etc/nginx/sites-enabled/skotos_game.conf
 
 nginx -t  # Verify everything parses correctly
-nginx -s reload
 
 ####
 # 7. Set up Tunnel
@@ -243,14 +298,14 @@ cat >/usr/local/websocket-to-tcp-tunnel/config.json <<EndOfMessage
             "name": "skotosgame",
             "listen": 10801,
             "send": 10443,
-            "host": "localhost",
+            "host": "$FQDN_CLIENT",
             "sendTunnelInfo": false
         },
         {
             "name": "skotos-tree-of-woe",
             "listen": 10802,
             "send": 10090,
-            "host": "localhost",
+            "host": "$FQDN_CLIENT",
             "sendTunnelInfo": false
         }
     ]
@@ -271,16 +326,17 @@ crontab -u skotos ~skotos/crontab.txt
 mkdir -p /var/www/html
 git clone https://github.com/ChatTheatre/orchil /var/www/html/client
 
-cat >>/var/www/html/client/profiles.js <<EndOfMessage
+cat >/var/www/html/client/profiles.js <<EndOfMessage
 "use strict";
 // orchil/profiles.js
 var profiles = {
         "portal_gables":{
                 "method":   "websocket",
                 "protocol": "ws",
-                "server":   "localhost", //"chat.gables.chattheatre.com",
+                "server":   "$FQDN_CLIENT", //"chat.gables.chattheatre.com",
                 "port":      10800,
                 "woe_port":  10802,
+                "http_port": 10080,
                 "path":     "/gables",
                 "extra":    "",
                 "reports":   false,
@@ -290,26 +346,90 @@ var profiles = {
 EndOfMessage
 
 #####
+## 9. Set up MariaDB for Thin-Auth
+#####
+
+mysql --user=root <<EndOfMessage
+CREATE DATABASE userdb;
+CREATE USER 'userdb'@'localhost' IDENTIFIED BY '$USERPASSWORD';
+GRANT ALL ON userdb.* TO 'userdb'@'localhost';
+EndOfMessage
+
+#####
 ## 25. Set up Thin-Auth
 #####
 #
-# For thin-auth, add UDF variables for DBPASSWORD and PAYPALACCOUNT, plus
+# For thin-auth, add UDF variable for DBPASSWORD, plus
 # all the game-specific stuff including your logo, etc. Hard to say if those
 # could be UDF variables or if they need to be a different fork of thin-auth.
 #
 ## thin-auth provides a SkotOS UserDB and full authentication facilities.
 ## It is normally prod-only, and *must* be installed into /var/www/html/user
-git clone https://github.com/ChatTheatre/thin-auth /var/www/html/user
-cp /var/www/html/user/config/database.json-SAMPLE /var/www/html/user/config/database.json
-cp /var/www/html/user/config/financial.json-SAMPLE /var/www/html/user/config/financial.json
-cp /var/www/html/user/config/general.json-SAMPLE /var/www/html/user/config/general.json
-cp /var/www/html/user/config/server.json-SAMPLE /var/www/html/user/config/server.json
+git clone $THINAUTH_GIT_URL /var/www/html/user
+cat >/var/www/html/user/config/database.json <<EndOfMessage
+{
+
+    "dbName": "userdb",
+    "dbUser": "userdb",
+    "dbPass": "$USERPASSWORD"
+}
+EndOfMessage
+
+cat >/var/www/html/user/config/financial.json <<EndOfMessage
+{
+
+    "paypalAcct": "$EMAIL",
+    "basicMonth": "12.95",
+    "basicQuarter": "34.95",
+    "basicYear": "129.95",
+    "premiumMonth": "29.95",
+    "premiumQuarter": "79.95",
+    "premiumYear": "299.95",
+    "premiumToBasicConversion": "2.3",
+    "monthlyPremiumSPs": "50",
+    "spMaxBasic": "50",
+    "spCostBasic": "34.95",
+    "spMaxPremium": "250",
+    "spCostPremium": "24.95"
+    "royalties": {
+    }
+
+}
+EndOfMessage
+
+cat >/var/www/html/user/config/general.json <<EndOfMessage
+{
+
+    "gameID": "gables",
+    "siteLogo": "gables-small.jpg",
+    "siteName": "Gables",
+    "userdbURL": "$FQDN_LOGIN",
+    "webURL": "$FQDN_CLIENT:10080/gables/gables.htm",
+    "woeURL": "$FQDN_CLIENT:10080/gables/TreeOfWoe.html",
+    "gameURL": "$FQDN_CLIENT",
+    "supportEmail": "$EMAIL"
+}
+EndOfMessage
+
+cat >/var/www/html/user/config/server.json <<EndOfMessage
+{
+
+    "serverIP": "0.0.0.0",
+    "serverAuthPort": 9970,
+    "serverCtlPort": 9971
+}
+EndOfMessage
+
+####
+# Finished
+####
+nginx -s reload
+touch ~/standup_finished_successfully.txt
 
 ####
 # See Also
 ####
 
-# "Setting Up a SkotOS Host": https://hackmd.io/L_6wWAXVRamgCWFPG9SqTw
 # https://github.com/ChatTheatre/thin-auth/blob/master/README.md
 # https://github.com/ChatTheatre/orchil
 
@@ -317,7 +437,5 @@ cp /var/www/html/user/config/server.json-SAMPLE /var/www/html/user/config/server
 # 754. Stuff that isn't done yet
 ####
 
-# * UFW setup
-# * Thin-Auth, including configuring its JSON files and its assets, game name, etc.
 # * Asset server for images, etc.
 # * Backups of any kind
