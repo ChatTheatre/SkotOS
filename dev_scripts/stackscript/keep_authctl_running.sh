cf43eeec1d75cb255883d24b5b33b14eec1af36d
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-01-27 11:53:08 +0000

    Linode: Try a different bash approach to restarting userdb-authctl running while redirecting output to a logfile

--- dev_scripts/stackscript/keep_authctl_running.sh
+++ dev_scripts/stackscript/keep_authctl_running.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ps aux | grep userdb-authctl | grep -v grep || /usr/bin/perl /var/skotos/dev_scripts/stackscript/userdb-authctl >/var/log/userdb-authctl.txt &
+if ps aux | grep userdb-authctl | grep -v grep
+then
+	echo "userdb-authctl is already running"
+else
+	echo "userdb-authctl not running - restarting"
+	/usr/bin/perl /var/skotos/dev_scripts/stackscript/userdb-authctl >/var/log/userdb-authctl.txt &
+fi
