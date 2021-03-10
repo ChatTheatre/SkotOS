78c2bdba9cd1aeafc9cc4f326be8f6902c184d69
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-10 16:09:55 +0000

    Add server-side theming back

--- skoot/usr/TextIF/obj/user.c
+++ skoot/usr/TextIF/obj/user.c
@@ -916,11 +916,6 @@ void parse_command(object body, string str) {
    MAIN->command(this_object(), body, str);
 }
 
-void initialize_theme()
-{
-}
-
-/*
 void initialize_theme() {
    string bgcolour, fgcolour, link, vlink, alink, str;
 
@@ -955,7 +950,6 @@ void initialize_theme() {
       html_message(str);
    }
 }
-*/
 
 /*
  * Variable to handle the output trickling.
