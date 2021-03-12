8fc8db20b52febe9b441ee61c34b7e8ca3caab91
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-12 12:55:19 +0000

    Better explanatory text when waiting for bootup

--- deploy_scripts/shared/wait_for_full_boot.sh
+++ deploy_scripts/shared/wait_for_full_boot.sh
@@ -6,7 +6,7 @@ set -x
 sleep 5
 while true
 do
-    echo "Checking if SkotOS is responsive yet. The first run can take five to ten minutes until everything is working..."
+    echo "Checking if SkotOS is responsive yet. The first run can take five to ten minutes until everything is working. Curl should get a 404 (not found) from this URL until fully booted, then a 200."
     curl -L --fail http://localhost:10080/FullyBooted > /tmp/test_skotos.html
     if [ $? -eq 0 ]; then
         break
