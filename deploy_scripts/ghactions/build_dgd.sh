b01e3ca8d7e34acc6e7143d50db95b1b41559c3d
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-12 12:10:37 +0000

    Fix DGD build path

--- deploy_scripts/ghactions/build_dgd.sh
+++ deploy_scripts/ghactions/build_dgd.sh
@@ -5,5 +5,5 @@ set -x
 
 cd $GITHUB_WORKSPACE
 git clone https://github.com/ChatTheatre/dgd ./dgd
-cd dgd
+cd dgd/src
 make DEFINES='-DUINDEX_TYPE="unsigned int" -DUINDEX_MAX=UINT_MAX -DEINDEX_TYPE="unsigned short" -DEINDEX_MAX=USHRT_MAX -DSSIZET_TYPE="unsigned int" -DSSIZET_MAX=1048576' install
