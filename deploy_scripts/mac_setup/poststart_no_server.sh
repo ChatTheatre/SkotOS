0c6cc93249cd07cc9a365deb05e7e125f2da5a3c
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-04-21 19:22:36 +0100

    WIP: use GAME_ROOT vs SKOTOS_ROOT appropriately in poststart deploy script

--- deploy_scripts/mac_setup/poststart_no_server.sh
+++ deploy_scripts/mac_setup/poststart_no_server.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+# cd to the SkotOS root directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPT_DIR
+cd ../..
+SKOTOS_ROOT="$(pwd)"
+
 if [ -z "$GAME_ROOT" ]
 then
     echo "You must set a GAME_ROOT to run this startup script."
@@ -11,9 +17,8 @@ fi
 cd "$GAME_ROOT"
 
 # Wait until SkotOS is booted and responsive
-./deploy_scripts/shared/wait_for_full_boot.sh
+"$SKOTOS_ROOT/deploy_scripts/shared/wait_for_full_boot.sh"
 
-# We killed Wafer up-top, so we can just run it here.
 echo "Running Wafer (auth server)"
 pushd wafer
 bundle
