174b328b5b512d2794ec4c778ba2ab3250b9513f
Sarah Tackett <sarahtackett@gmail.com>
2022-02-12 15:38:10 -0500

    Testing using nodejs v14

--- deploy_scripts/stackscript/linode_stackscript.sh
+++ deploy_scripts/stackscript/linode_stackscript.sh
@@ -186,7 +186,7 @@ apt-get install git nginx-full cron bison build-essential -y
 
 # Websocket-to-tcp-tunnel requirements
 # curl -sL https://deb.nodesource.com/setup_9.x | bash -
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt install nodejs npm -y -o Debug::pkgProblemResolver=true -o Debug::Acquire::http=true
 
 # Thin-auth requirements
